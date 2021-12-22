d7795b6bbf06cfd8aea36517999c32937e0c12bb
quantm <012kinglight@gmail.com>
2021-12-22 12:43:21 +0700

    remove unused file-fix db connection

--- resources/views/auth/users/login.blade.php
+++ resources/views/auth/users/login.blade.php
@@ -12,48 +12,6 @@
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors"/>
 
-        {{--        <form method="POST" action="{{ route('dang-nhap') }}">
-                @csrf
-
-                    <!-- Email Address -->
-                    <div>
-                        <x-input id="email" placeholder="EMAIL" class="text-sm block mt-1 w-full" type="email" name="email"
-                                 :value="old('email')" required autofocus/>
-                    </div>
-
-                    <!-- Password -->
-                    <div class="mt-4">
-                        <x-input id="password" class="text-sm block mt-1 w-full"
-                                 type="password"
-                                 name="password"
-                                 placeholder="MẬT KHẨU"
-                                 required autocomplete="password"/>
-                    </div>
-
-                    <!-- Remember Me -->
-                    <div class="block mt-4">
-                        <label for="remember_me" class="inline-flex items-center">
-                            <input id="remember_me" type="checkbox"
-                                   class="square border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                   name="remember">
-                            <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                        </label>
-                    </div>
-
-                    <div class="flex items-center justify-end mt-4">
-                        @if (Route::has('password.request'))
-                            <a class="underline text-sm text-gray-600 hover:text-gray-900"
-                               href="{{ route('password.request') }}">
-                                {{ __('Forgot your password?') }}
-                            </a>
-                        @endif
-
-                        <x-button class="ml-3">
-                            {{ __('Log in') }}
-                        </x-button>
-                    </div>
-                </form>--}}
-
         <form method="POST" action="{{ route('dang-nhap') }}" class="mt-8">
             @csrf
             <div class="py-2" x-data="{ show: true }">
@@ -67,6 +25,7 @@
                         </svg>
                     </div>
                     <input placeholder="EMAIL"
+                           value="{{ old('email') }}"
                            type="email"
                            id="email"
                            name="email"
@@ -111,8 +70,9 @@
             <div class="pb-1">
                 <label class="block text-gray-500 font-bold my-4">
                     <input type="checkbox" class="w-5 h-5 rounded-full"
-                           id="remember_me"
+                           id="remember"
                            name="remember"
+                           value=1
                            checked
                     />
                     <span class="ml-ml-2 text-small text-gray-600">{{ __('Remember me') }}</span>
